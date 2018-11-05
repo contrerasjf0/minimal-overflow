52fa8c2b4a141de658a605b0fb5069fe94618c99
contrerasjf <contrerasjf.0@hotmail.com>
2018-11-04 23:00:26 -0600

    update(questionDetail): Implement the question model
    * The question model was implemented.
    * The CardModule component was implemented.
    * A base template was added to the question-detail html file.
    * In the QuestionDetail component class the question  model  was implemented.

--- src/app/questions/question-detail.component.ts
+++ src/app/questions/question-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Question } from './question.model';
 
 @Component({
     selector: 'app-question-detail',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
 })
 
 export class QuestionDetailComponent{
-
+    question: Question = new Question(
+        'title',
+        'description',
+        new Date,
+        'devicon-android-plain'
+    );
 }
