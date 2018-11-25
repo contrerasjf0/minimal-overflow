5f1189f69fe0b1d0b57ad5c775899be0d076297a
contrerasjf <contrerasjf.0@hotmail.com>
2018-11-25 08:52:57 -0600

    backend:feature(route):Implement the enpoint for create question
    * The endpoint for create question was implemented with post method.
    * The endpoint for retrieve the question was refactored to search the question by ID.
    * The library body-parse was installed.

--- server/routes/question.js
+++ server/routes/question.js
@@ -10,9 +10,9 @@ const question = {
   icon: 'devicon-android-plain',
   answers: [],
   user: {
-    firstName: 'Sacha',
-    lastName: 'Lifszyc',
-    email: 'sacha@platzi.com',
+    firstName: 'Frank',
+    lastName: 'Esp',
+    email: 'frank@rtsty.com',
     password: '123456'
   }
 }
@@ -23,6 +23,27 @@ const questions = new Array(10).fill(question)
 app.get('/', (req, res) => res.status(200).json(questions))
 
 // /api/questions/:id
-app.get('/:id', (req, res) => res.status(200).json(question))
+app.get('/:id', (req, res) => {
+  const { id } = req.params
+  const q = questions.find(({ _id }) => _id === +id)
+  res.status(200).json(q)
+})
+
+// POST /api/questions
+app.post('/', (req, res) => {
+  const question = req.body
+  question._id = +new Date()
+  question.user = {
+    email: 'frank@rtsty.com',
+    password: '123456',
+    firstName: 'Frank',
+    lastName: 'Esp'
+  }
+  question.createdAt = new Date()
+  question.answers = []
+  questions.push(question)
+  res.status(201).json(question)
+})
+
 
 export default app
