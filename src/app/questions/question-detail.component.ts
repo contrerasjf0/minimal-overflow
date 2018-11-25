1dd9e0e85cc4da532a047bdaa56a081fc63845cf
contrerasjf <contrerasjf.0@hotmail.com>
2018-11-25 08:31:22 -0600

    frontend:update(questionDetail):Implement the question service
    * The question services was implemented in the question-detail component to retrieve the question by ID.
    
    Resolve: c39

--- src/app/questions/question-detail.component.ts
+++ src/app/questions/question-detail.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Question } from './question.model';
+import { QuestionService } from './question.service';
+import { ActivatedRoute } from '@angular/router';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
     selector: 'app-question-detail',
     templateUrl: './question-detail.component.html',
-    styleUrls: ['./question-detail.component.scss']
+    styleUrls: ['./question-detail.component.scss'],
+    providers: [QuestionService]
 })
 
-export class QuestionDetailComponent{
-    question: Question = new Question(
-        'title',
-        'description',
-        new Date,
-        'devicon-android-plain'
-    );
+export class QuestionDetailComponent implements OnInit, OnDestroy{
+    question?: Question;ç
+    loading =  true;
+    sub: any;
+    constructor(
+        private questionService: QuestionService,
+        private route: ActivatedRoute
+    ){}
+
+    ngOnInit(){
+        this.sub = this.route
+                    .params.subscribe((params => {
+                        this.questionService.getQuestion(params.id)
+                            .then((question: Question) => {
+                                this.question = question;
+                                this.loading =false
+                            });
+                    }));
+    }
+
+    ngOnDestroy(){
+        this.sub.unsubscribe();
+    }
 }
