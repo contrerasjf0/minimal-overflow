32c7f68c2a82abf8a752bcc8d41e7bc92583c794
contrerasjf <contrerasjf.0@hotmail.com>
2018-11-26 07:01:45 -0600

    frontend:feature(authServices):Implement the auth server
    * The auth server was implemented to implement the methods that will be used fo the features, signin, logout, signup, etc..

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import urljoin from 'url-join';
+import { environment } from '../../environments/environment';
+import { User } from './user.model';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { map, catchError } from'rxjs/operators';
+import { Observable } from 'rxjs';
+
+
+@Injectable()
+export class AuthService {
+  
+  usersUrl: string;
+  currentUser?: User;
+
+  constructor(private http: HttpClient) {
+
+    this.usersUrl = urljoin(environment.apiUrl, 'auth');
+
+    if (this.isLoggedIn()) {
+      const { userId, email, firstName, lastName } = JSON.parse(localStorage.getItem('user'));
+      this.currentUser = new User(email, null, firstName, lastName, userId);
+    }
+
+  }
+  signin(user: User): Observable<any> {
+
+    const body = JSON.stringify(user);
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    return this.http.post(urljoin(this.usersUrl, 'signin'), body, { headers })
+                .pipe(
+                    map((response: any) => {
+                        const json = response.json();
+                        this.login(json);
+                        return json;
+                      }),
+                      catchError((error: Response) => Observable.throw(error.json()))
+                    );
+  }
+
+  login = ({ token, userId, firstName, lastName, email }) => {
+
+    this.currentUser = new User(email, null, firstName, lastName, userId);
+    
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify({ userId, firstName, lastName, email }));
+  }
+
+  isLoggedIn() {
+    return localStorage.getItem('token') !== null;
+  }
+
+}
